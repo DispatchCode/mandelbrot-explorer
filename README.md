7f8059fd2737e3659e08751dd193c1edd3b81d83
DispatchCode <crivellari.marco90@gmail.com>
2022-04-24 01:42:07 +0200

    refactoring + README update

--- README.md
+++ README.md
@@ -1,1 +1,133 @@
 # Mandelbrot Fractal explorer
+
+Inspired by my own [fraCtal](https://github.com/DispatchCode/fraCtal) project; code & formulas explanations are there too.
+Information about the mandelbrot set can also be found on [Wikipedia](https://en.wikipedia.org/wiki/Mandelbrot_set).
+
+For better performance build & run with release flag 😎:
+
+```
+cargo run --release
+```
+The code produced by Rust heavily makes use of the MMX set, as you can see down below (`mandelbrot_fractal()` function):
+
+![2022-04-24_011503](https://user-images.githubusercontent.com/4256708/164949367-02cce677-968e-4012-b3b0-ad4f4360bd83.png)
+
+### Features:
+✅ Mandelbrot Fractal <br>
+✅ SDL2 (you need SDL2 to run the project) <br>
+✅ save the generated fractal as image (PNG) <br>
+✅ size, zoom, colors can bhe changed <br>
+❌ ImGui support (or a GUI menu where options can be changed) <br>
+❌ multithreading support
+
+### Future plans:
+🎯 ImGui support <br>
+
+---
+## Generated images
+
+```Rust
+let (val_in, val_out) = (32, 0);
+let g = if i >= 32 {
+    (255.0
+        - (((i + val_in) as f32 / (max_iterations + val_in) as f32)
+            * (255 - val_out) as f32)) as u8
+} else {
+    0 as u8
+};
+```
+
+![mandelbrot0](https://user-images.githubusercontent.com/4256708/164943526-16890613-b342-43c4-9453-102be268403b.png)
+
+---
+
+```Rust
+let attenuation = false; // disable attenuation
+// ...
+let (val_in, val_out) = (32, 150);
+let g = if i >= 32 {
+    (255.0
+        - (((i + val_in) as f32 / (max_iterations + val_in) as f32)
+            * (255 - val_out) as f32)) as u8
+} else {
+    0 as u8
+};
+```
+![mandelbrot](https://user-images.githubusercontent.com/4256708/164946372-5da42f27-840b-4196-8d3f-8d4e501fcd8c.png)
+
+---
+
+```Rust
+let attenuation = false;
+
+// ...
+
+let (val_in, val_out) = (32, 0);
+let g = if i >= 32 {
+    (255.0
+        - (((i + val_in) as f32 / (max_iterations + val_in) as f32)
+            * (255 - val_out) as f32)) as u8
+} else {
+    0 as u8
+};
+```
+![mandelbrot](https://user-images.githubusercontent.com/4256708/164946435-5a032e9c-258c-49fe-9269-cf138484aeec.png)
+
+---
+
+```Rust
+let attenuation = false;
+
+// ...
+
+let (val_in, val_out) = (32, 0);
+let g = if i >= 0 {
+    (255.0
+        - (((i + val_in) as f32 / (max_iterations + val_in) as f32)
+            * (255 - val_out) as f32)) as u8
+} else {
+    0 as u8
+};
+```
+
+![mandelbrot](https://user-images.githubusercontent.com/4256708/164946477-f2c95510-e42e-4755-8caa-b10c4554b613.png)
+
+---
+```Rust
+let attenuation = true;
+
+let (blu_a, blu_b) = (255.0, 255.0);
+let (red_a, red_b) = (0.0, 255.0);
+
+let (val_in, val_out) = (128, 0);
+let g = if i >= 32 {
+    (255.0
+        - (((i + val_in) as f32 / (max_iterations + val_in) as f32)
+            * (255 - val_out) as f32)) as u8
+} else {
+    0 as u8
+};
+```
+
+![mandelbrot](https://user-images.githubusercontent.com/4256708/164946619-e0fe9141-23ee-4b8c-bf0c-7d3e85248396.png)
+
+---
+```Rust
+let attenuation = true;
+let (x_min, x_max, y_min, y_max) = (0.40, 0.37, 0.26, 0.21);
+
+// The colors around the lake
+let (blu_a, blu_b) = (32.0, 255.0);
+let (red_a, red_b) = (32.0, 255.0);
+
+let (val_in, val_out) = (180,0);
+let g = if i >= 128 {
+    (255.0
+        - (((i + val_in) as f32 / (max_iterations + val_in) as f32)
+          * (255 - val_out) as f32)) as u8
+    } else {
+        32 as u8
+};
+```
+
+![mandelbrot](https://user-images.githubusercontent.com/4256708/164948369-3117a0e7-f6c3-4789-8064-c29917ec3b3c.png)
--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,36 @@
 use num_complex::Complex;
-use std::fs::File;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use std::fs::File;
 
 fn main() {
-    display_fractal();
+    let (width, height) = (1920, 1080);
+    // Create the fractal
+    let buffer = mandelbrot_fractal(width, height);
+
+    save_image(&buffer, width, height);
+    display_fractal(buffer, width, height);
 }
-
-fn mandelbrot_fractal(width : usize, height : usize) -> Vec<u8> {
-    let mut buffer = vec![0; width * height * 3];
-    let mut c = Complex::new(0.0, 0.0);
-    let mut z = Complex::new(0.0, 0.0);
-    let max_iterations = 1000;
-    let escape_radius = 2.0;
-    let (x_min, x_max, y_min, y_max) = (-2.0, 1.0, -1.0, 1.0);
-
-    let x_step = (x_max - x_min) / width as f64;
-    let y_step = (y_max - y_min) / height as f64;
-
-    for y in 0..height {
-        for x in 0..width {
-            c.re = x_min + x as f64 * x_step;
-            c.im = y_min + y as f64 * y_step;
-            z.re = 0.0;
-            z.im = 0.0;
-
-            let mut i = 0;
-            while i < max_iterations && z.norm_sqr() < escape_radius {
-                z = z * z + c;
-                i += 1;
-            }
-
-            let (blu_a, blu_b)  = (0.0, 255.0);
-            let (red_a, red_b)  = (0.0, 255.0);
-
-            let rstepx = (red_b - red_a) / width as f32;
-            let bstepy = (blu_b - blu_a) / height as f32;
-
-            let r = (red_a + rstepx*x as f32) as u8;
-            let b = (blu_a + bstepy*y as f32) as u8;
-
-            let val_in = 32;
-            let val_out = 0;
-            let g= if i >= 48 { (255.0 - (((i + val_in) as f32 / (max_iterations + val_in) as f32) * (255 - val_out) as f32 )) as u8} else {0 as u8};
-
-            let atten =  i as f32 / max_iterations as f32;
-            let atten = 1.0 - atten*atten;
-
-            let r = (r as f32 * atten) as u8;
-            let b = (b as f32 * atten) as u8;
-
-            let index = (y * width + x) * 3;
-            buffer[index + 0] = r;
-            buffer[index + 1] = g;
-            buffer[index + 2] = b;
-        }
-    }
-
-    let mut file = File::create("../images/mandelbrot.png").unwrap();
-    let mut encoder = png::Encoder::new(&mut file, width as u32, height as u32);
-    encoder.set_color(png::ColorType::Rgb);
-    encoder.set_depth(png::BitDepth::Eight);
-    let mut writer = encoder.write_header().unwrap();
-    writer.write_image_data(&buffer).unwrap();
-
-    buffer
-}
-
-fn display_fractal() {
-
+/*
+ * Display the mandelbrot fractal
+ */
+fn display_fractal(buffer: Vec<u8>, width: usize, height: usize) {
+    // Init an SDL2 context
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
+    // Create a Window...
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 1920, 1080)
+        .window("Mandelbrot Fractal Explorer", width as u32, height as u32)
         .position_centered()
         .vulkan()
         .build()
-
         .unwrap();
 
-    //window.set_fullscreen(sdl2::video::FullscreenType::Desktop);
-
+    // ...and get a canvas to draw on it
     let mut canvas = window.into_canvas().build().unwrap();
-   // let size = canvas.window().size();
-    let (width, height) = (1920, 1080);
 
+    // Create a texture from the canvas
     let texture_creator = canvas.texture_creator();
     let mut texture = texture_creator
         .create_texture_streaming(
@@ -95,9 +40,10 @@ fn display_fractal() {
         )
         .unwrap();
 
-    let buffer = mandelbrot_fractal(width as usize, height as usize);
-    texture.update(None, &buffer, (width * 3) as usize).unwrap();
+    // Update the texture with the pixel buffer
+    texture.update(None, &buffer, width * 3).unwrap();
 
+    // Display it, and loop until the user closes the window
     canvas.clear();
     canvas.copy(&texture, None, None).unwrap();
     canvas.present();
@@ -115,4 +61,90 @@ fn display_fractal() {
             }
         }
     }
+}
+
+/*
+ * Save the fractal to a file
+ */
+fn mandelbrot_fractal(width: usize, height: usize) -> Vec<u8> {
+    println!("Generating fractal...");
+    // Buffer where pixels are stored
+    let mut buffer: Vec<u8> = vec![0; width * height * 3];
+    // Complex numbers
+    let mut c = Complex::new(0.0, 0.0);
+    let mut z = Complex::new(0.0, 0.0);
+
+    // the higher, the more accurate
+    let max_iterations = 2000;
+    let escape_radius = 4.0;
+
+    // Whether attenuation is enabled
+    let attenuation = true;
+
+    // Zoom that you can change to look at different parts of the fractal
+    let (x_min, x_max, y_min, y_max) = (-2.0, 1.0, -1.0, 1.0);
+    //let (x_min, x_max, y_min, y_max) = (0.40, 0.37, 0.26, 0.21);
+
+    // The colors around the lake
+    let (blu_a, blu_b) = (0.0, 255.0);
+    let (red_a, red_b) = (0.0, 255.0);
+
+    let rstepx = (red_b - red_a) / width as f32;
+    let bstepy = (blu_b - blu_a) / height as f32;
+
+    let x_step = (x_max - x_min) / width as f64;
+    let y_step = (y_max - y_min) / height as f64;
+
+    for y in 0..height {
+        for x in 0..width {
+            c.re = x_min + x as f64 * x_step;
+            c.im = y_min + y as f64 * y_step;
+            z.re = 0.0;
+            z.im = 0.0;
+
+            let mut i: u32 = 0;
+            while i < max_iterations && z.norm_sqr() < escape_radius {
+                z = z * z + c;
+                i += 1;
+            }
+
+            let r = (red_a + rstepx * x as f32) as u8;
+            let b = (blu_a + bstepy * y as f32) as u8;
+
+            let (val_in, val_out) = (32, 0);
+            let g = if i >= 32 {
+                (255.0
+                    - (((i + val_in) as f32 / (max_iterations + val_in) as f32)
+                        * (255 - val_out) as f32)) as u8
+            } else {
+                0 as u8
+            };
+
+            let atten = i as f32 / max_iterations as f32;
+            let atten = 1.0 - atten * atten;
+            let atten = if attenuation { atten } else { 1.0 };
+
+            let r = (r as f32 * atten) as u8;
+            let b = (b as f32 * atten) as u8;
+
+            let index = (y * width + x) * 3;
+            buffer[index + 0] = r;
+            buffer[index + 1] = g;
+            buffer[index + 2] = b;
+        }
+    }
+
+    buffer
+}
+
+/*
+ * Save the fractal to a PNG file
+ */
+fn save_image(buffer: &Vec<u8>, width: usize, height: usize) {
+    let mut file = File::create("mandelbrot.png").unwrap();
+    let mut encoder = png::Encoder::new(&mut file, width as u32, height as u32);
+    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+    writer.write_image_data(buffer).unwrap();
 }
